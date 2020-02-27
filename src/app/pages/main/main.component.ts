227c763ebf19f3eb244acbebd95042551beee356
AndersonCastellon <anderson.ivan.fuentes@gmail.com>
2020-02-26 21:28:35 -0600

    Limpieza del proyecto

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -1,11 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/firebase/auth.service';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html'
 })
 export class MainComponent implements OnInit {
-  constructor() {}
+  constructor(private auth: AuthService) {}
 
   ngOnInit() {}
+
+  logout() {
+    this.auth.logout();
+  }
+
+  switchView() {}
 }
